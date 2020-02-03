7ef3f9dea64f6e28624b34001879c3361caea95c
thomaslahaye-etpa <thomas_lahaye@outlook.fr>
2020-02-03 14:40:28 +0100

    choix par numéro
    jdvelh exercice 1 : choix par numéro

---
+++ jdvelh.c
@@ -0,0 +1,21 @@
+// Afficher 2 destinations et pouvoir les choisir par leur numéro. Suite au choix, annoncer la destination comme atteinte.
+#include <stdio.h>
+
+int main(){
+	int playerChoice;
+
+printf ("Vous vous trouvez seul dans le froid, au milieu d'une plaine déserte. Derrière vous, une pierre maculée de sang séché. Vous avez dû vous cogner la tête dessus, mais impossible de vous souvenir de quoi que ce soit. Devant vous, deux chemins s'offrent à vous : \n");
+printf ("L'un, droit et bien entretenu, serpente vers l'énorme château qui trône sur la colline d'en face. Les fenêtres laissent filtrer de fins rayons de lumière : l'endroit semble habité. \n");
+printf ("L'autre, plus sinueux, mène vers une forêt, rendue insondable par l'obscurité environnante. Les pins vous dominent de leurs silhouettes menaçantes, et vous percevez le lointain écho des cris des créatures qui la peuplent. \n");
+printf ("Que faites-vous ? \n");
+printf ("(1) Vous prenez la route du château. \n (2) Vous vous dirigez vers la forêt. \n");
+scanf("%d", playerChoice);
+if (playerChoice==1){
+	printf("Vous vous élancez vers le château, bien déterminé à en rencontrer les occupants. \n")
+};
+else if (playerChoice==2){
+	printf("Vous avancez d'un pas mal assuré vers la forêt, et vous enfoncez sous le couvert des arbres. \n")
+};
+};
+
+// FIN EX 1 //
